f82b6f89c4e640670a2d12c8d7f995b680db0906
FallenMaster <MASTER>
2019-05-27 23:21:08 +0300

    base commit

---
+++ client/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+   <div class="header">
+      <router-link
+         v-for="link, i in links"
+         class="header-link"
+         tag="a"
+         :to="link.url"
+         :key="`headerLink_${i}`">{{link.title}}</router-link>
+   </div>
+</template>
+
+<script lang="ts">
+   import { Component, Prop, Vue } from 'vue-property-decorator';
+
+   @Component
+   export default class HeaderMenu extends Vue {
+      public links: Array<{ url: string, title: string }> = [
+         { url: '/page1', title: 'page 1' },
+         { url: '/page2', title: 'page 2' },
+      ];
+   }
+</script>
+
+<style scoped>
+   .header {
+      background-color: #1A9DDB;
+      text-align: center;
+      padding: 12px;
+      color: white;
+   }
+   .header-link {
+      padding: 12px;
+      color: white;
+   }
+</style>
---
+++ client/src/views/Page2.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+      {{text}}
+   </div>
+</template>
+
+<script lang="ts">
+   import {Component, Prop, Vue} from 'vue-property-decorator';
+
+   @Component
+   export default class Page2 extends Vue {
+      public text: string = 'Another text';
+   }
+</script>
+
+<style scoped>
+
+</style>
